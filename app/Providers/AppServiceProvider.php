67f49a3512f9ddf652b999a0130cc84587a6d140
Wasabi199 <xxryan0909@gmail.com>
2024-01-11 21:11:52 +0800

    provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,9 +27,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        if (App::environment('production')) {
-            URL::forceScheme('https');
-        }
+        // if (App::environment('production')) {
+        //     URL::forceScheme('https');
+        // }
         Schema::defaultStringLength(191);
     }
 }
