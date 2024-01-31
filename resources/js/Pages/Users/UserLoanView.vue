db71d0c1cfb91d95c8da314d851010b53a49ea34
Wasabi199 <xxryan0909@gmail.com>
2024-01-31 19:22:40 +0800

    redirect

--- resources/js/Pages/Users/UserLoanView.vue
+++ resources/js/Pages/Users/UserLoanView.vue
@@ -2,7 +2,7 @@
     <AppLayout>
         <div class="flex justify-between bg-white shadow">
             <div class="flex items-center px-4 py-2 mx-auto max-w-7xl sm:px-6 lg:px-8">
-            <Link :href="route('userDashboard')">
+            <Link :href="route('loansView')">
                 <h1 @click="loan()" class="text-xl font-extrabold leading-tight text-gray-800 cursor-pointer">
                     Loans
                 </h1>
