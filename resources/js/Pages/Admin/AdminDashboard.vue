f2c0d4b5b644d72174c9d0b2932fc783e372f74b
Wasabi199 <xxryan0909@gmail.com>
2024-02-04 21:40:44 +0800

    add

--- resources/js/Pages/Admin/AdminDashboard.vue
+++ resources/js/Pages/Admin/AdminDashboard.vue
@@ -1,34 +1,32 @@
 <template>
   <AppLayout title="MITRF ADMIN">
     <template #header>
-      <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+      <h2 class="text-xl font-semibold leading-tight text-gray-800">
         Homepage
       </h2>
     </template>
 
     <div class="py-12">
-      <div class="max-w-screen-2xl mx-auto sm:px-6 lg:px-8">
-        <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+      <div class="mx-auto max-w-screen-2xl sm:px-6 lg:px-8">
+        <div class="overflow-hidden bg-white shadow-xl sm:rounded-lg">
           <div>
             <button @click="viewOfficer = !viewOfficer"
-            class="m-5 p-2 shadow-md rounded-lg border-2
-            hover:border-red-700 hover:text-yellow-800 focus:border-red-700 focus:text-yellow-800">
+            class="p-2 m-5 border-2 rounded-lg shadow-md hover:border-red-700 hover:text-yellow-800 focus:border-red-700 focus:text-yellow-800">
                 View MITRF Inc. Officers
             </button>
             <div
-              class="flex justify-items-center p-6 sm:px-20 bg-white border-b border-gray-200"
+              class="flex p-6 bg-white border-b border-gray-200 justify-items-center sm:px-20"
             >
               <div  class="">
                 <table v-if="viewOfficer"
-                  class="border-separate rounded-md shadow-xl border-spacing-2 border border-slate-500 mt-12 bg-red-800
-                  text-yellow-500 border-spacing-y-4 bg-opacity-90 drop-shadow-lg"
+                  class="mt-12 text-yellow-500 bg-red-800 border border-separate rounded-md shadow-xl border-spacing-2 border-slate-500 border-spacing-y-4 bg-opacity-90 drop-shadow-lg"
                 >
                   <thead class="text-lg">
                     <tr>
                       <th class="">MITRF Inc. Officers</th>
-                      <th class="flex block justify-between">
+                      <th class="flex justify-between block">
 
-                          <div class=" text-sm mt-1 underline">
+                          <div class="mt-1 text-sm underline ">
                               <p>Updated last: <span>{{new Date(this.boards.updated_at).toLocaleDateString()}}</span></p>
                           </div>
                           <svg
@@ -62,7 +60,7 @@
                       <td class="font-bold whitespace-nowrap">
                         Benefits Committee Chairman
                       </td>
-                      <td class=" ">{{this.boards.benefits_chairman}}</td>
+                      <td class="">{{this.boards.benefits_chairman}}</td>
                     </tr> -->
                     <tr>
                       <td class="font-bold whitespace-nowrap">Vice Chairman</td>
@@ -76,25 +74,25 @@
                     </tr>
                     <tr>
                       <td class="font-bold whitespace-nowrap">Treasurer</td>
-                      <td class=" ">{{this.boards.treasurer}}</td>
+                      <td class="">{{this.boards.treasurer}}</td>
                     </tr>
                     <tr>
                       <td class="font-bold whitespace-nowrap">
                         Internal Auditor
                       </td>
-                      <td class=" ">{{this.boards.internal_auditor}}</td>
+                      <td class="">{{this.boards.internal_auditor}}</td>
                     </tr>
                     <tr>
                       <td class="font-bold whitespace-nowrap">
                         MITRF Accountant
                       </td>
-                      <td class=" ">{{this.boards.accountant}}</td>
+                      <td class="">{{this.boards.accountant}}</td>
                     </tr>
                     <tr>
                       <td class="font-bold whitespace-nowrap">
                         School Physician
                       </td>
-                      <td class=" ">{{this.boards.school_physician}}</td>
+                      <td class="">{{this.boards.school_physician}}</td>
                     </tr>
                   </tbody>
                 </table>
@@ -120,10 +118,10 @@
       @close="updateBoardsModal = !updateBoardsModal"
     >
       <div class="p-5">
-        <div class="flex justify-between text-xl font-bold text-gray-900 my-3">
+        <div class="flex justify-between my-3 text-xl font-bold text-gray-900">
           <div>Update Board</div>
           <svg
-            class="h-6 w-6 cursor-pointer"
+            class="w-6 h-6 cursor-pointer"
             fill="none"
             stroke="currentColor"
             viewBox="0 0 24 24"
@@ -141,43 +139,43 @@
         <div class="p-5 space-y-5">
             <div>
                 <JetLabel>MITRF Chairman</JetLabel>
-                <JetInput class="w-full border-2 border-black py-1 pl-1" v-model="this.updateBoards.chairman" :placeholder="this.boards.chairman" required/>
+                <JetInput class="w-full py-1 pl-1 border-2 border-black" v-model="this.updateBoards.chairman" :placeholder="this.boards.chairman" required/>
             </div>
             <div>
                 <JetLabel>Credit Committee Chairman</JetLabel>
-                <JetInput class="w-full border-2 border-black py-1 pl-1" v-model="this.updateBoards.committee_chairman" :placeholder="this.boards.committee_chairman" required/>
+                <JetInput class="w-full py-1 pl-1 border-2 border-black" v-model="this.updateBoards.committee_chairman" :placeholder="this.boards.committee_chairman" required/>
             </div>
             <!-- <div>
                 <JetLabel>Benefits Committee Chairman</JetLabel>
-                <JetInput class="w-full border-2 border-black py-1 pl-1" v-model="this.updateBoards.benefits_chairman" :placeholder="this.boards.benefits_chairman" required/>
+                <JetInput class="w-full py-1 pl-1 border-2 border-black" v-model="this.updateBoards.benefits_chairman" :placeholder="this.boards.benefits_chairman" required/>
             </div> -->
             <div>
                 <JetLabel>Vice Chairman</JetLabel>
-                <JetInput class="w-full border-2 border-black py-1 pl-1" v-model="this.updateBoards.vice_chairman" :placeholder="this.boards.vice_chairman" required/>
+                <JetInput class="w-full py-1 pl-1 border-2 border-black" v-model="this.updateBoards.vice_chairman" :placeholder="this.boards.vice_chairman" required/>
             </div>
             <div>
                 <JetLabel>Corporate Secretary</JetLabel>
-                <JetInput class="w-full border-2 border-black py-1 pl-1" v-model="this.updateBoards.corporate_secretary" :placeholder="this.boards.corporate_secretary" required/>
+                <JetInput class="w-full py-1 pl-1 border-2 border-black" v-model="this.updateBoards.corporate_secretary" :placeholder="this.boards.corporate_secretary" required/>
             </div>
             <div>
                 <JetLabel>Treasurer</JetLabel>
-                <JetInput class="w-full border-2 border-black py-1 pl-1" v-model="this.updateBoards.treasurer" :placeholder="this.boards.treasurer" required/>
+                <JetInput class="w-full py-1 pl-1 border-2 border-black" v-model="this.updateBoards.treasurer" :placeholder="this.boards.treasurer" required/>
             </div>
             <div>
                 <JetLabel>Internal Auditor</JetLabel>
-                <JetInput class="w-full border-2 border-black py-1 pl-1" v-model="this.updateBoards.internal_auditor" :placeholder="this.boards.internal_auditor" required/>
+                <JetInput class="w-full py-1 pl-1 border-2 border-black" v-model="this.updateBoards.internal_auditor" :placeholder="this.boards.internal_auditor" required/>
             </div>
             <div>
                 <JetLabel>MITRF Accountant</JetLabel>
-                <JetInput class="w-full border-2 border-black py-1 pl-1" v-model="this.updateBoards.accountant" :placeholder="this.boards.accountant" required/>
+                <JetInput class="w-full py-1 pl-1 border-2 border-black" v-model="this.updateBoards.accountant" :placeholder="this.boards.accountant" required/>
             </div>
             <div>
                 <JetLabel>School Physician</JetLabel>
-                <JetInput class="w-full border-2 border-black py-1 pl-1" v-model="this.updateBoards.school_physician" :placeholder="this.boards.school_physician" required/>
+                <JetInput class="w-full py-1 pl-1 border-2 border-black" v-model="this.updateBoards.school_physician" :placeholder="this.boards.school_physician" required/>
             </div>
         </div>
-        <div class="w-full flex justify-center">
-            <button @click="submit()" class="py-2 px-5 border-2 border-green-500 rounded-lg text-green-500 hover:bg-green-500 hover:text-white">UPDATE</button>
+        <div class="flex justify-center w-full">
+            <button @click="submit()" class="px-5 py-2 text-green-500 border-2 border-green-500 rounded-lg hover:bg-green-500 hover:text-white">UPDATE</button>
         </div>
 
       </div>
@@ -225,6 +223,15 @@ export default {
   },
   methods:{
     submit(){
+      this.updateBoards.chairman = this.updateBoards.chairman !=''? this.updateBoards.chairman: this.boards.chairman
+      this.updateBoards.committee_chairman = this.updateBoards.committee_chairman !=''? this.updateBoards.committee_chairman: this.boards.committee_chairman
+      this.updateBoards.vice_chairman = this.updateBoards.vice_chairman !=''? this.updateBoards.vice_chairman: this.boards.vice_chairman
+      this.updateBoards.corporate_secretary = this.updateBoards.corporate_secretary !=''? this.updateBoards.corporate_secretary: this.boards.corporate_secretary
+      this.updateBoards.treasurer = this.updateBoards.treasurer !=''? this.updateBoards.treasurer: this.boards.treasurer
+      this.updateBoards.internal_auditor = this.updateBoards.internal_auditor !=''? this.updateBoards.internal_auditor: this.boards.internal_auditor
+      this.updateBoards.accountant = this.updateBoards.accountant !=''? this.updateBoards.accountant: this.boards.accountant
+      this.updateBoards.school_physician = this.updateBoards.school_physician !=''? this.updateBoards.school_physician: this.boards.school_physician
+      
         this.updateBoards.post(route("updateBoards"))
         this.updateBoardsModal =! this.updateBoardsModal;
     }
